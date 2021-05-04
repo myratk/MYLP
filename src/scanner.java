a28bf4352c32f0eb13cb97c563abc210dad9cf20
Myra Khokhar <u1975631@unimail.hud.ac.uk>
2021-05-04 03:18:19 +0100

    Initial commit

---
+++ src/scanner.java
@@ -0,0 +1,90 @@
+
+ 
+import java_cup.runtime.*;
+/* import sym; */
+ 
+public class scanner {
+  /* single lookahead character */
+  protected static int next_char;
+ 
+  /* advance input by one character */
+  protected static void advance()
+    throws java.io.IOException
+    { next_char = System.in.read(); }
+ 
+  /* initialize the scanner */
+  public static void init()
+    throws java.io.IOException
+    { advance(); }
+ 
+  /* recognize and return the next complete token */
+  public static Symbol next_token()
+    throws java.io.IOException
+    {
+      for (;;)
+        switch (next_char)
+          {
+            case '0': case '1': case '2': case '3': case '4':
+            case '5': case '6': case '7': case '8': case '9':
+              /* parse a decimal integer */
+              int i_val = 0;
+              do {
+                i_val = i_val * 10 + (next_char - '0');
+                advance();
+              } while (next_char >= '0' && next_char <= '9');
+            return new Symbol(sym.INT, new Integer(i_val));
+
+            case 'a': case 'b': case 'c': case 'd': case 'e':
+            case 'f': case 'g': case 'h': case 'i': case 'j':
+            case 'k': case 'l': case 'm': case 'n': case 'o':
+            case 'p': case 'q': case 'r': case 's': case 't':
+            case 'u': case 'v': case 'w': case 'x': case 'y':
+            case 'z': case 'A': case 'B': case 'C': case 'D':
+            case 'E': case 'F': case 'G': case 'H': case 'I':
+            case 'J': case 'K': case 'L': case 'M': case 'N':
+            case 'O': case 'P': case 'Q': case 'R': case 'S':
+            case 'T': case 'U': case 'V': case 'W': case 'X':
+            case 'Y': case 'Z':
+              String fromchar;
+              String inputString = "";
+              do {
+                fromchar = "" + (char) next_char;
+                inputString += fromchar;
+                advance();
+              } while ((next_char>='a'&& next_char <='z') || (next_char>='A' && next_char<='Z'));
+            return switch (inputString) {
+              case "print" -> new Symbol(sym.PRINT);
+              case "call" -> new Symbol(sym.CALL);
+              case "do" -> new Symbol(sym.DO);
+              case "while" -> new Symbol(sym.WHILE);
+              case "if" -> new Symbol(sym.IF);
+              case "then" -> new Symbol(sym.THEN);
+              case "var" -> new Symbol(sym.VARDEC);
+              case "START" -> new Symbol(sym.START);
+              case "FINISH" -> new Symbol(sym.FINISH);
+              case "end" -> new Symbol(sym.END);
+              case "function" -> new Symbol(sym.FUNCTION);
+              default -> new Symbol(sym.ID, inputString);
+            };
+
+            case '=': advance(); return new Symbol(sym.ASSIGNS);
+            case ';': advance(); return new Symbol(sym.SEMI);
+            case '+': advance(); return new Symbol(sym.PLUS);
+            case '-': advance(); return new Symbol(sym.MINUS);
+            case ',': advance(); return new Symbol(sym.COMMA);
+            case '*': advance(); return new Symbol(sym.TIMES);
+            case '/': advance(); return new Symbol(sym.DIVIDE);
+            case '(': advance(); return new Symbol(sym.LPAREN);
+            case ')': advance(); return new Symbol(sym.RPAREN);
+            case '>': advance(); return new Symbol(sym.GT);
+ 
+            case -1: return new Symbol(sym.EOF);
+ 
+            default:
+              /* in this simple scanner we just ignore everything else */
+              advance();
+            break;
+          }
+    }
+};
+
